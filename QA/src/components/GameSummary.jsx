8053c1a43d3f528f3ca66daced1fbf6ba6a715b1
Morfeo <gastongomez2014@hotmail.com>
2025-07-03 23:25:53 -0300

    Se agregaron los componentes principales para poder empezar a trabajar, el juego ya es funcional

---
+++ QA/src/components/GameSummary.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import AnimatedContainer from './AnimatedContainer.jsx';
+
+function GameSummary({ correctAnswers, totalQuestions, onRestart, transitionType }) {
+  const percentage = (correctAnswers / totalQuestions) * 100;
+
+  return (
+    <AnimatedContainer
+      transitionType={transitionType}
+      className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center"
+    >
+      <h2 className="text-2xl font-bold mb-4">¡Juego Terminado!</h2>
+      <div className="text-4xl font-bold text-blue-600 mb-4">
+        {percentage.toFixed(0)}%
+      </div>
+      <p className="mb-6 text-gray-600">
+        Respondiste correctamente {correctAnswers} de {totalQuestions} preguntas
+      </p>
+      <button
+        onClick={onRestart}
+        className="bg-blue-200 text-blue-950 cursor-pointer px-6 py-2 rounded-lg hover:bg-blue-950 hover:text-blue-200 transition-colors duration-300"
+      >
+        Jugar de nuevo
+      </button>
+    </AnimatedContainer>
+  );
+}
+
+export default GameSummary;
---
+++ QA/src/components/QuestionCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import AnimatedContainer from './AnimatedContainer';
+
+function QuestionCard({
+  question,
+  onAnswer,
+  isAnswered,
+  selectedAnswer,
+  transitionType,
+}) {
+  return (
+    <AnimatedContainer
+      transitionType={transitionType}
+      className=' bg-white p-6 rounded-md shadow-lg max-w-2xl w-full'
+    >
+      <h2 className='text-xl font-semibold mb-4 text-black'>
+        {question.question}
+      </h2>
+      <div className='grid grid-rows-2 grid-cols-2 gap-3'>
+        {question.options.map((optionItem, index) => (
+          <button
+            key={index}
+            onClick={() => !isAnswered && onAnswer(optionItem)}
+            className={`w-full h-full p-3 text-left cursor-pointer  rounded-lg border-gray-500 transition-colors hover:scale-105  duration-500 ${
+              isAnswered
+                ? optionItem.correct
+                  ? 'bg-green-200 border-green-500'
+                  : selectedAnswer?.option === optionItem.option
+                  ? 'bg-red-200 border-red-500'
+                  : 'bg-gray-100'
+                : 'hover:bg-blue-50 bg-gray-50'
+            } border ${
+              isAnswered &&
+              (optionItem.correct || selectedAnswer?.option === optionItem.option)
+                ? 'border-2'
+                : 'border-2'
+            }`}
+            disabled={isAnswered}
+          >
+            {optionItem.option}
+          </button>
+        ))}
+      </div>
+    </AnimatedContainer>
+  );
+}
+
+export default QuestionCard;
